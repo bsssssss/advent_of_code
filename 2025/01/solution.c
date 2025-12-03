c5f83dcee6bb301a88f6bc6b3d960118f6d20e1a
Bss <mx.bss@proton.me>
2025-12-03 19:34:47 +0100

    day1: found a solution

--- 2025/01/solution.c
+++ 2025/01/solution.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv) {
-    // open the file passed as argument
-    FILE* file = fopen(argv[1], "r");
+int main(int argc, char **argv) {
 
-    // file error handling
+    FILE *file = fopen(argv[1], "r");
+
     if (file == NULL) {
         fprintf(stderr, "could not open file\n");
         return 1;
     }
 
-    // init
     char line[256];
-    int  position   = 50;
-    int  zero_count = 0;
-    int  line_count = 0;
+    int position = 50;
+    int zero_count = 0;
 
-    // parse lines
     while (fgets(line, sizeof(line), file)) {
-        // skip empty lines
         if (line[0] == '\n') {
             continue;
         }
 
-        // printf("line %d has %lu characters\n", line_count + 1, strlen(line));
-
         char direction = line[0];
-        char value[20];
+        char jump_by_str[strlen(line) + 1];
 
-        strncpy(value, line + 1, strlen(line));
-        value[strlen(line)] = '\0';
+        strcpy(jump_by_str, line + 1);
+        int jump_by = atoi(jump_by_str);
 
-        printf("direction: %c, value: %s\n", direction, value);
-        line_count++;
+        if (direction == 'L') {
+            position = (position - jump_by) % 100;
+        }
+        else if (direction == 'R') {
+            position = (position + jump_by) % 100;
+        }
+
+        if (position == 0) {
+            zero_count++;
+        }
+
     }
-
-    // close the file
     fclose(file);
-    printf("Done.\n");
 
+    printf("password is %d\n", zero_count);
     return 0;
 }
