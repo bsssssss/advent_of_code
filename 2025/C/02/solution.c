05e5d1cf7349c4a8d21244605581ef8703a55e82
Bss <mx.bss@proton.me>
2025-12-07 00:21:41 +0100

    day2: continue

--- 2025/C/02/solution.c
+++ 2025/C/02/solution.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void process_range(char* range)
+{
+    printf("range : %s\n", range);
+
+    long start_id;
+    long end_id;
+    char** endptr;
+
+    start_id = strtol(strsep(&range, "-"), endptr, 10);
+    end_id   = strtol(range, endptr, 10);
+
+    printf("start : %lu\n", start_id);
+    printf("end   : %lu\n\n", end_id);
+}
 
 int main(int argc, char* argv[])
 {
@@ -8,6 +25,19 @@ int main(int argc, char* argv[])
     if (file_handle == NULL) {
         printf("error: couldn't open file");
         return 1;
+    }
+
+    char  buf_line[1024];
+    char* line = fgets(buf_line, sizeof(buf_line), file_handle);
+    buf_line[strcspn(buf_line, "\n")] = '\0';
+
+    char* range_token;
+    char* range_sep = ",";
+
+    range_token = strsep(&line, range_sep);
+    while (range_token != NULL) {
+        process_range(range_token);
+        range_token = strsep(&line, range_sep);
     }
 
     fclose(file_handle);
