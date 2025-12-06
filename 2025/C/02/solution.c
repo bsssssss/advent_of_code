d762b084c3ecaff01381cf1b8d7f43f1b98835c5
Bss <mx.bss@proton.me>
2025-12-06 19:30:50 +0100

    day2: struggle

--- 2025/C/02/solution.c
+++ 2025/C/02/solution.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) 
+int main(int argc, char* argv[])
 {
+    FILE* file_handle;
+    file_handle = fopen("input.txt", "r");
+
+    if (file_handle == NULL) {
+        printf("error: couldn't open file");
+        return 1;
+    }
+
+    fclose(file_handle);
+    return 0;
 }
