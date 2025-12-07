708cbe56cd5998f05f9e7a72dc6380ff554fd555
Bss <mx.bss@proton.me>
2025-12-07 17:40:05 +0100

    day2: found a solution

--- 2025/C/02/solution.c
+++ 2025/C/02/solution.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void process_id(long id)
+int is_invalid_id(long id)
 {
     char id_str[256];
     sprintf(id_str, "%ld", id);
@@ -16,35 +16,45 @@ void process_id(long id)
     }
 
     if (length % 2 == 0) {
-        char first_half[128];
-        char second_half[128];
-        strncpy(first_half, id_str, (length / 2) - 1);
-        strncpy(second_half, id_str + (length / 2) - 1, (length / 2) - 1);
+        char first_half_str[256];
+        char second_half_str[256];
 
-        printf("first: %s, second: %s\n", first_half, second_half);
+        strncpy(first_half_str, id_str, (length / 2));
+        first_half_str[length / 2] = '\0';
+
+        strncpy(second_half_str, id_str + (length / 2), (length / 2));
+        second_half_str[length / 2] = '\0';
+
+        long first_half  = strtol(first_half_str, NULL, 10);
+        long second_half = strtol(second_half_str, NULL, 10);
+
+        if (first_half == second_half) {
+            return 0;
+        }
     }
+    return -1;
 }
 
-void process_range(char* range)
+long process_range(char* range)
 {
-
     printf("range : %s\n", range);
 
     long start_id = strtol(strsep(&range, "-"), NULL, 10);
     long end_id   = strtol(range, NULL, 10);
-
-    printf("start : %ld\n", start_id);
-    printf("end   : %ld\n", end_id);
+    long result   = 0;
 
     for (long id = start_id; id <= end_id; id++) {
-        process_id(id);
+        if (is_invalid_id(id) != -1) {
+            result += id;
+        }
     }
+    return result;
 }
 
 int main(int argc, char* argv[])
 {
     FILE* file_handle;
-    file_handle = fopen("test.txt", "r");
+    file_handle = fopen("input.txt", "r");
 
     if (file_handle == NULL) {
         printf("error: couldn't open file");
@@ -53,17 +63,18 @@ int main(int argc, char* argv[])
 
     char  buf_line[1024];
     char* line = fgets(buf_line, sizeof(buf_line), file_handle);
-
     buf_line[strcspn(buf_line, "\n")] = '\0';
 
-    char* range_token;
-    char* range_sep = ",";
+    char* range_sep   = ",";
+    char* range_token = strsep(&line, range_sep);
+    long  result      = 0;
 
-    range_token = strsep(&line, range_sep);
     while (range_token != NULL) {
-        process_range(range_token);
+        result += process_range(range_token);
         range_token = strsep(&line, range_sep);
     }
+
+    printf("password: %ld\n", result);
 
     fclose(file_handle);
     return 0;
