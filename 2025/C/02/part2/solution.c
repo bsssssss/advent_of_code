9860409962c571a62a3556fe405460e7c7cc42eb
Bss <mx.bss@proton.me>
2025-12-07 20:33:09 +0100

    day2 part2: continue

--- 2025/C/02/part2/solution.c
+++ 2025/C/02/part2/solution.c
@@ -1,33 +1,25 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int is_invalid_id(long id)
+#define MAX_LONG_STR 32
+#define MAX_LINE_LEN 4096
+
+bool is_invalid_id(long id)
 {
-    char id_str[256];
+    char id_str[MAX_LONG_STR];
 
     sprintf(id_str, "%ld", id);
     unsigned long length = strlen(id_str);
 
-    // if (length % 2 == 0) {
-    //     char first_half_str[256];
-    //     char second_half_str[256];
-    //
-    //     strncpy(first_half_str, id_str, (length / 2));
-    //     first_half_str[length / 2] = '\0';
-    //
-    //     strncpy(second_half_str, id_str + (length / 2), (length / 2));
-    //     second_half_str[length / 2] = '\0';
-    //
-    //     long first_half  = strtol(first_half_str, NULL, 10);
-    //     long second_half = strtol(second_half_str, NULL, 10);
-    //
-    //     if (first_half == second_half) {
-    //         return 0;
-    //     }
-    // }
+    if (length < 2) {
+        return false;
+    }
 
-    return -1;
+    printf("testing %s\n", id_str);
+
+    return false;
 }
 
 long process_range(char* range)
@@ -39,10 +31,11 @@ long process_range(char* range)
     long result = 0;
 
     for (long id = start_id; id <= end_id; id++) {
-        if (is_invalid_id(id) != -1) {
+        if (is_invalid_id(id)) {
             result += id;
         }
     }
+
     return result;
 }
 
@@ -56,13 +49,13 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    char  buf_line[1024];
+    char  buf_line[MAX_LINE_LEN];
     char* line = fgets(buf_line, sizeof(buf_line), file_handle);
 
     buf_line[strcspn(buf_line, "\n")] = '\0';
 
-    char* range_sep = ",";
-    char* range_token = strsep(&line, range_sep);
+    const char* range_sep = ",";
+    char*       range_token = strsep(&line, range_sep);
 
     long result = 0;
 
@@ -72,7 +65,7 @@ int main(int argc, char* argv[])
     }
 
     printf("password: %ld\n", result);
-
     fclose(file_handle);
+
     return 0;
 }
