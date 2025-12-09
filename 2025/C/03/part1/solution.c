4067b52c7ccb7a5873ccb69f27d3952ab98e93d6
Bss <mx.bss@proton.me>
2025-12-09 19:16:58 +0100

    day3 part1: found a solution

--- 2025/C/03/part1/solution.c
+++ 2025/C/03/part1/solution.c
@@ -2,10 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int calculate_bank_joltage(char* bank)
+typedef struct Bank
 {
-    int joltage = 0;
-    int bank_length = strlen(bank);
+    char* string;
+    int   length;
+} bank_t;
+
+int max_value_position_in_array(int* array, int length)
+{
+    int maximum = 0;
+    int position = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (array[i] > maximum) {
+            maximum = array[i];
+            position = i;
+        }
+    }
+    return position;
+}
+
+void bank_to_values(bank_t bank, int* values)
+{
+    for (int i = 0; i < bank.length; i++) {
+        char str[2];
+        strncpy(str, bank.string + i, 1);
+        values[i] = atoi(str);
+    }
+}
+
+int calculate_bank_joltage(bank_t bank, int n)
+{
+    printf("processing bank (%s)\n", bank.string);
+
+    char joltage_str[n + 1];
+    int  joltage = 0;
+
+    for (int i = 0; i < n; i++) {
+        int values[bank.length];
+        bank_to_values(bank, values);
+
+        int max_pos =
+            max_value_position_in_array(values, bank.length - ((n - 1) - i));
+
+        joltage_str[i] = bank.string[max_pos];
+        bank.string = bank.string + (max_pos + 1);
+        bank.length = strlen(bank.string);
+    }
+
+    joltage_str[n] = '\0';
+    joltage = atoi(joltage_str);
+
+    printf("\tjoltage for this bank is %d\n", joltage);
+    printf("\n");
 
     return joltage;
 }
@@ -13,7 +62,7 @@ int calculate_bank_joltage(char* bank)
 int main(int argc, char* argv[])
 {
     FILE* file_handle;
-    file_handle = fopen("test.txt", "r");
+    file_handle = fopen("input.txt", "r");
 
     if (file_handle == NULL) {
         printf("error: couldn't open file\n");
@@ -21,13 +70,21 @@ int main(int argc, char* argv[])
     }
 
     char line[4096];
-    int result = 0;
+    int  result = 0;
 
     while (fgets(line, sizeof(line), file_handle)) {
-        // strip out new line char at the end
+
+        if (line[0] == '\n') {
+            continue;
+        }
+
         line[strcspn(line, "\n")] = '\0';
 
-        int bank_joltage = calculate_bank_joltage(line);
+        bank_t bank;
+        bank.string = strdup(line);
+        bank.length = strlen(bank.string);
+
+        int bank_joltage = calculate_bank_joltage(bank, 2);
         result += bank_joltage;
     }
 
