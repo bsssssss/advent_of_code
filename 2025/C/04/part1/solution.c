608aa576f05b32a5da37ad367e55056fa95c0c35
Bss <mx.bss@proton.me>
2025-12-10 23:26:45 +0100

    day4 part1: little bit

--- 2025/C/04/part1/solution.c
+++ 2025/C/04/part1/solution.c
@@ -1,7 +1,20 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) 
+typedef struct Grid_row
+{
+    const char* symbols;
+    int         length;
+} row_t;
+
+int calculate_accessible(row_t row)
+{
+    uint32_t result = 0;
+    return result;
+}
+
+int main(int argc, char* argv[])
 {
     FILE* fh = fopen("input.txt", "r");
 
@@ -10,13 +23,22 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    char line[4096];
+    char     line[4096];
+    uint32_t result = 0;
+
     while (fgets(line, sizeof(line), fh)) {
         if (line[0] == '\n') {
             continue;
         }
+
         line[strcspn(line, "\n")] = '\0';
-        printf("%s\n", line);
+
+        row_t row;
+        row.symbols = line;
+        row.length = strlen(line);
+
+        uint32_t accessible_rolls = calculate_accessible(row);
+        result += accessible_rolls;
     }
 
     fclose(fh);
