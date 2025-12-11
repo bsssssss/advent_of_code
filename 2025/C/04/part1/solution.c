7e596556b984861f5330da957f22ec79e2618d25
Bss <mx.bss@proton.me>
2025-12-11 12:05:02 +0100

    day4 part1: made a good structure for the grid approach

--- 2025/C/04/part1/solution.c
+++ 2025/C/04/part1/solution.c
@@ -1,46 +1,115 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct Grid_row
-{
-    const char* symbols;
-    int         length;
-} row_t;
+#define INPUT_FILE "input.txt"
 
-int calculate_accessible(row_t row)
+typedef struct {
+    char* contents[200];
+    int   rows;
+    int   cols;
+} Grid;
+
+void Grid_free(Grid* grid)
 {
-    uint32_t result = 0;
-    return result;
+    if (!grid) return;
+
+    for (int i = 0; i < grid->rows; i++) {
+        free(grid->contents[i]);
+    }
+
+    free(grid);
+}
+
+int Grid_add_row(Grid* grid, char* row)
+{
+    if (grid->rows >= 200) {
+        printf("grid is full (max 200 rows\n");
+        return -1;
+    }
+
+    grid->contents[grid->rows] = strdup(row);
+
+    if (!grid->contents[grid->rows]) {
+        printf("error: couldn't add row to Grid\n");
+        return -1;
+    }
+
+    if (grid->cols == 0) {
+        grid->cols = strlen(row);
+    }
+
+    grid->rows++;
+
+    return 0;
+}
+
+void Grid_print(Grid* grid)
+{
+    for (int i = 0; i < grid->rows; i++) {
+        printf("%s\n", grid->contents[i]);
+    }
+}
+
+Grid* Grid_create()
+{
+    Grid* grid = malloc(sizeof(Grid));
+    if (!grid) {
+        printf("error: couldn't allocate memory for Grid\n");
+        return NULL;
+    }
+
+    grid->rows = 0;
+    grid->cols = 0;
+
+    return grid;
+}
+
+Grid* read_file(char* file)
+{
+    FILE* fh = fopen(file, "r");
+    if (fh == NULL) {
+        printf("error: couldn't open file\n");
+        return NULL;
+    }
+
+    Grid* grid = Grid_create();
+
+    if (!grid) {
+        return NULL;
+    }
+
+    char buffer[4096];
+    while (fgets(buffer, sizeof(buffer), fh)) {
+
+        if (buffer[0] == '\n') {
+            continue;
+        }
+
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if (Grid_add_row(grid, buffer) != 0) {
+            Grid_free(grid);
+            fclose(fh);
+            return NULL;
+        }
+    }
+
+    fclose(fh);
+    return grid;
 }
 
 int main(int argc, char* argv[])
 {
-    FILE* fh = fopen("input.txt", "r");
+    Grid* grid = read_file(INPUT_FILE);
 
-    if (fh == NULL) {
-        printf("error: couldn't open file\n");
+    if (!grid) {
         return 1;
     }
 
-    char     line[4096];
-    uint32_t result = 0;
+    Grid_print(grid);
 
-    while (fgets(line, sizeof(line), fh)) {
-        if (line[0] == '\n') {
-            continue;
-        }
-
-        line[strcspn(line, "\n")] = '\0';
-
-        row_t row;
-        row.symbols = line;
-        row.length = strlen(line);
-
-        uint32_t accessible_rolls = calculate_accessible(row);
-        result += accessible_rolls;
-    }
-
-    fclose(fh);
+    Grid_free(grid);
     return 0;
 }
