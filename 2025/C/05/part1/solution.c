a38c687b31b51a3afb36731730428439f42f73e6
Bss <mx.bss@proton.me>
2025-12-13 12:59:46 +0100

    day5 part1: found a solution

--- 2025/C/05/part1/solution.c
+++ 2025/C/05/part1/solution.c
@@ -1,21 +1,29 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define INPUT_FILE "test.txt"
+#define INPUT_FILE "input.txt"
+#define MAX_ROWS 2048
 
 typedef struct {
-    int start;
-    int end;
+    uint64_t start;
+    uint64_t end;
 } Range;
 
 typedef struct {
-    Range ranges[200]; 
-} Puzzle;
+    Range    ranges[MAX_ROWS];
+    uint64_t ids[MAX_ROWS];
+    int      rcount;
+    int      icount;
+} Database;
 
-void die(char* message)
+void database_free(Database* db);
+
+void die(Database* db, char* message)
 {
+    database_free(db);
     if (errno) {
         perror(message);
     }
@@ -24,47 +32,110 @@ void die(char* message)
     }
 }
 
-void read_input_file()
+void database_fill(Database* db)
 {
     FILE* fh = fopen(INPUT_FILE, "r");
 
     if (!fh) {
-        die("could not open file");
+        die(db, "could not open file");
     }
 
     char*   line = NULL;
     size_t  len = 0;
     ssize_t read;
 
+    int stage = 0;
     while ((read = getline(&line, &len, fh) != -1)) {
 
-        int stage = 0;
         if (line[0] == '\n') {
             stage++;
-        }
-
-        if (stage == 0) {
-            // parse ranges
-        }
-        else if (stage == 1) {
-            // parse indices
-        }
-        else {
-            die("oops");
+            continue;
         }
 
         line[strcspn(line, "\n")] = '\0';
+
+        if (stage == 0) {
+            uint64_t start = 0, end = 0;
+            if (sscanf(line, "%llu-%llu", &start, &end) == 2) {
+                Range rg = { .start = start, .end = end };
+                db->ranges[db->rcount] = rg;
+                db->rcount++;
+            }
+        }
+        else if (stage == 1) {
+            uint64_t id = strtoll(line, NULL, 10);
+            db->ids[db->icount] = id;
+            db->icount++;
+        }
+        else {
+            die(db, "oops");
+        }
     }
 
     fclose(fh);
     free(line);
 }
 
+void database_print(Database* db)
+{
+    for (int i = 0; i < db->rcount; i++) {
+        printf("range %d:\n", i);
+        printf("\tstart : %llu\n\tend   : %llu\n", db->ranges[i].start, db->ranges[i].end);
+    }
+    printf("\n");
+
+    for (int i = 0; i < db->icount; i++) {
+        printf("ID %d: %llu\n", i, db->ids[i]);
+    }
+    printf("\n");
+}
+
+void database_free(Database* db)
+{
+    if (db) {
+        free(db);
+    }
+}
+
+Database* database_init()
+{
+    Database* db = malloc(sizeof(Database));
+
+    if (!db) {
+        die(NULL, "Could not allocate memory for database\n");
+    }
+
+    db->rcount = 0;
+    db->icount = 0;
+
+    return db;
+}
+
+bool is_fresh(Database* db, uint64_t id)
+{
+    for (int i = 0; i < db->rcount; i++) {
+        if (id >= db->ranges[i].start && id <= db->ranges[i].end) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char* argv[])
 {
-    read_input_file();
+    Database* db = database_init();
+    database_fill(db);
 
-    int fresh_count = 0;
-    // printf("%d fresh ingredients\n", fresh_count);
+    // database_print(db);
+
+    int fresh_c = 0;
+    for (int i = 0; i < db->icount; i++) {
+        if (is_fresh(db, db->ids[i])) {
+            fresh_c++;
+        }
+    }
+    printf("%d fresh ingredients\n", fresh_c);
+
+    database_free(db);
     return 0;
 }
