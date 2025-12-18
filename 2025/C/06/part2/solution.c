478f33b8c1a14a4db4ccd1cec355d1c10844e223
Bss <mx.bss@proton.me>
2025-12-18 19:41:34 +0100

    day6 part2: trying things

--- 2025/C/06/part2/solution.c
+++ 2025/C/06/part2/solution.c
@@ -3,21 +3,36 @@
 #include <stdlib.h>
 
 #define INPUT_FILE "test.txt"
+#define MAX_LEN 4096
+
+typedef struct {
+    char** contents[MAX_LEN][MAX_LEN];
+    size_t len;
+} Grid;
 
 typedef struct {
     char*  raw;
     size_t raw_len;
+    Grid*  grid;
 } Puzzle;
 
 /*****************************************************************************/
 
-int     read_input_file(Puzzle* pz, char* filename);
+void    read_input_file(Puzzle* pz, char* filename);
 void    puzzle_destroy(Puzzle* pz);
 Puzzle* puzzle_create(char* filename);
 
 /*****************************************************************************/
 
-int read_input_file(Puzzle* pz, char* filename)
+void mk_grid(Puzzle* pz)
+{
+    char cptr = *pz->raw;
+    while (cptr != '\0') {
+        cptr++;
+    }
+}
+
+void read_input_file(Puzzle* pz, char* filename)
 {
     FILE* fh = fopen(filename, "rb");
     check(fh, "could not open file");
@@ -25,6 +40,7 @@ int read_input_file(Puzzle* pz, char* filename)
     fseek(fh, 0, SEEK_END);
     long sz = ftell(fh);
     rewind(fh);
+
     debug("size of %s: %ld bytes", filename, sz);
 
     // + 1 for null byte
@@ -35,18 +51,23 @@ int read_input_file(Puzzle* pz, char* filename)
     pz->raw[pz->raw_len] = '\0';
 
     fclose(fh);
-    return 0;
+    return;
 
 error:
     if (fh) fclose(fh);
-    if (pz->raw) free(pz->raw);
-    return -1;
+    puzzle_destroy(pz);
+    return;
 }
 
 void puzzle_destroy(Puzzle* pz)
 {
     if (pz) {
-        if (pz->raw) free(pz->raw);
+        if (pz->raw) {
+            free(pz->raw);
+        }
+        if (pz->grid) {
+            free(pz->grid);
+        }
         free(pz);
     }
 }
@@ -56,10 +77,15 @@ Puzzle* puzzle_create(char* filename)
     Puzzle* pz = malloc(sizeof(Puzzle));
     check_mem(pz);
 
-    pz->raw = NULL;
+    pz->raw     = NULL;
     pz->raw_len = 0;
 
+    pz->grid      = malloc(sizeof(Grid));
+    pz->grid->len = 0;
+
     read_input_file(pz, filename);
+    mk_grid(pz);
+
     return pz;
 
 error:
@@ -78,13 +104,15 @@ void print_raw(Puzzle* pz)
 
 int main(void)
 {
-    Puzzle* pz = puzzle_create("bloo");
+    Puzzle* pz = puzzle_create(INPUT_FILE);
     check(pz, "failed to create puzzle");
+
     print_raw(pz);
 
     puzzle_destroy(pz);
     return 0;
 
 error:
+    puzzle_destroy(pz);
     return 1;
 }
